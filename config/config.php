a0d404633ccdda83f0f84429e72ac31646050fc8
Osama Elhamahmy <osama.eg@live.com>
2026-02-04 12:53:58 +0200

    - Add the latest BASE_URLS for both Morocco, and Qatar

--- config/config.php
+++ config/config.php
@@ -52,7 +52,7 @@ return [
    |--------------------------------------------------------------------------
    |
    | The region you registered in with PayTabs
-     you must pass value from this array ['ARE','EGY','SAU','OMN','JOR','GLOBAL']
+     you must pass value from this array ['ARE','EGY','SAU','OMN','JOR','IRQ','KWT','MAR','QAT','GLOBAL']
    |
    */
 
